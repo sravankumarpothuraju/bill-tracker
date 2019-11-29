61ee94f9565b2de48d6d93b461953ec75e3459b8
sravankumarpothuraju <sravan.pothuraju@gmail.com>
2019-11-28 23:55:48 -0500

    created addcategory component and passed data to App component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,38 @@
-import React from 'react';
-import './App.css';
+import React, { useState } from "react";
+
+import AddCategory from "./components/AddCategory.js";
+import NavBar from "./components/NavBar.js";
+import Chart from "./components/Chart.js";
+import BillsTable from "./components/BillsTable.js";
 
 function App() {
+  const [shouldShowAddCategory, setShouldShowAddCategory] = useState(true);
+  const [categories, setCategories] = useState([]);
+
+  const addCategory = category => {
+    setCategories([...categories, category]);
+    setShouldShowAddCategory(false);
+  };
+
   return (
     <div className="App">
-
+      {shouldShowAddCategory ? (
+        <AddCategory onSubmit={addCategory} />
+      ) : (
+        <div>
+          <NavBar />
+          <div className="container flex">
+            <div className="w-1/2">
+              <BillsTable />
+            </div>
+            <div className="w-1/2">
+              <Chart />
+            </div>
+          </div>
+        </div>
+      )}
     </div>
   );
 }
 
 export default App;
-
-
